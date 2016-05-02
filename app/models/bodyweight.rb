ec60ae19efb15eeec4a18de23adc9ce10eaf8e4a
Seth Baughman <sbaughman1@me.com>
2016-05-02 17:14:13 -0400

    move method to multiply bodyweight input by 10 to controller from model to fix issue with losing decimal place

--- app/models/bodyweight.rb
+++ app/models/bodyweight.rb
@@ -1,13 +1,6 @@
 class Bodyweight < ApplicationRecord
   belongs_to :prep
   validates :weight, presence: true, numericality: true
-  before_save :multiply_by_ten
-
-  private
-
-  def multiply_by_ten
-    self.weight = weight * 10
-  end
 
   def display_weight
     weight / 10.0
