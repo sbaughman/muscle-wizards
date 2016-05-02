2c40a13f10bdea03a8b14ed9813c050925d11d59
Seth Baughman <sbaughman1@me.com>
2016-05-02 15:33:39 -0400

    add bodyweight controller actions for index, new, create

--- app/controllers/bodyweights_controller.rb
+++ app/controllers/bodyweights_controller.rb
@@ -1,2 +1,26 @@
 class BodyweightsController < ApplicationController
+
+  def index
+    @weights = Bodyweight.all(created_at: :desc)
+  end
+
+  def new
+    @weight = Bodyweight.new
+  end
+
+  def create
+    @weight = Bodyweight.new(weight_params)
+    if @weight.save
+      flash[:success] = "Weigh-in successful"
+      redirect_to prep_path(@weight.prep)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def weight_params
+    params.require(:bodyweight).permit(:weight)
+  end
 end
--- app/models/bodyweight.rb
+++ app/models/bodyweight.rb
@@ -1,2 +1,16 @@
 class Bodyweight < ApplicationRecord
+  belongs_to :prep
+  validates :weight, presence: true, numericality: true
+  before_save :multiply_by_ten
+
+  private
+
+  def multiply_by_ten
+    self.weight = weight * 10
+  end
+
+  def display_weight
+    weight / 10.0
+  end
+
 end
