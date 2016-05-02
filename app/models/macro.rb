6f1828a540a339a11676704f1673492c1c8f0255
Seth Baughman <sbaughman1@me.com>
2016-05-02 17:54:37 -0400

    index, create, and new actions for macros. Index and new views for macros

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -1,4 +1,6 @@
 class Macro < ApplicationRecord
   belongs_to :prep
-  validates :protein, :carbs, :fat, :fiber, presence: true, numericality: true
+  validates :protein, :carbs, :fat, presence: true
+  validates :protein, :carbs, :fat, numericality: true
+  validates :fiber, numericality: true, allow_blank: true
 end
