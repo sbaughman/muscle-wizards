6495cdbf17cc444248afe7ee1c306591c870a3a4
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-03 17:01:49 -0400

    Generated devise views, added required fields to new user registration, removed bio from validation for users

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ApplicationRecord
   belongs_to :prep
-  validates :image, attachment_presence: true
+  validates :avatar, attachment_presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :preps
-  validates :name, :email, :gender, :age, :bio, :height, presence: true
+  validates :name, :email, :gender, :age, :height, presence: true
   validates :age, :height, numericality: true
   validates :email, uniqueness: true
   validate :email_is_valid_format
   validate :password_might_not_be_completely_terrible
   before_validation :downcase_email
-  before_save :set_default_avatar
+  # before_save :set_default_avatar
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
@@ -28,8 +28,8 @@ class User < ApplicationRecord
     errors.add(:password, "Password must contain at least four unique characters") unless self.password.split('').uniq.length >= 4
   end
 
-  def set_default_avatar
-     self.avatar ||= "https://unsplash.it/400/?image=1061"
-  end
+  # def set_default_avatar
+  #    self.avatar ||= "https://unsplash.it/400/?image=1061"
+  # end
 
 end
