14e2dd4edae29e23a280a595cac4ce7dd799ff46
Seth Baughman <sbaughman1@me.com>
2016-05-03 14:42:19 -0400

    new views for target_macros and target_cardios

--- app/models/target_macro.rb
+++ app/models/target_macro.rb
@@ -2,5 +2,5 @@ class TargetMacro < ApplicationRecord
   belongs_to :prep
   validates :protein, :carbs, :fat, presence: true
   validates :protein, :carbs, :fat, numericality: true
-  validates :fiber, numericality: true, allow_blank: truebelongs_to :prep
+  validates :fiber, numericality: true, allow_blank: true
 end
