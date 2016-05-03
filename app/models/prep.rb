b450846b4f5996cb408657169c65afea53c98954
Seth Baughman <sbaughman1@me.com>
2016-05-03 15:23:04 -0400

    adds target macros and target cardio to prep show

--- app/models/prep.rb
+++ app/models/prep.rb
@@ -2,6 +2,7 @@ class Prep < ApplicationRecord
   has_many :bodyweights
   has_many :photos
   has_many :cardios
+  has_many :target_cardios
   has_many :contests
   has_many :macros
   has_many :target_macros
--- app/models/target_macro.rb
+++ app/models/target_macro.rb
@@ -3,4 +3,8 @@ class TargetMacro < ApplicationRecord
   validates :protein, :carbs, :fat, presence: true
   validates :protein, :carbs, :fat, numericality: true
   validates :fiber, numericality: true, allow_blank: true
+
+  def total_calories
+    protein * 4 + carbs * 4 + fat * 9
+  end
 end
