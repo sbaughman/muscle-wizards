dbf70785afb583e7157507ed71e595c3adcd6e1b
Seth Baughman <sbaughman1@me.com>
2016-05-04 17:56:10 -0400

    added methods to seeds.rb for creating sample athlete, prep, contest, and 30 days worth of bodyweights

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,26 @@
 
 poses = ["Front Relaxed", "Side Relaxed", "Back Relaxed", "Front Double Biceps", "Side Chest", "Side Triceps", "Rear Lat Spread", "Back Double Biceps", "Front Abs & Thigh", "Most Muscular"]
 
+def picture_from_url(url)
+  URI.parse(url)
+end
+
 poses.each do |pose|
   Tag.create!(name: pose)
+end
+
+athlete = User.create!(email: "example@example.com", name: Faker::Name.name, gender: "M", age: 32, bio: Faker.hipster.sentence, height: 68, phone_number: 3175555555)
+
+prep = athlete.prep.create!(title: "2016 Summer Competition Season")
+
+prep.contest.create!(title: "WNBF Pro Bowl", date: (Time.now + 7257600))
+
+weight = 2050
+counter = 22592000
+30.times do
+  bw = prep.bodyweights.create!(weight: weight)
+  bw.created_at -= counter
+  bw.save!
+  weight -= rand(0..7)
+  counter -= 86400
 end
