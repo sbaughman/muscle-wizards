9833ae1ee98f19b368e802ac184b46b67864bf15
Seth Baughman <sbaughman1@me.com>
2016-05-04 14:01:08 -0400

    adds option to select pose tag on new photo creation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :prep
-  has_one :tag, through: :taggings
+  has_one :tag, through: :tagging
+  has_one :tagging, dependent: :destroy
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
