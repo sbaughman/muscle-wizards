675cc8e960ad98dadd166045255ccde8fb9b85bc
Seth Baughman <sbaughman1@me.com>
2016-05-04 13:26:30 -0400

    add tags for poses to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+poses = ["Front Relaxed", "Side Relaxed", "Back Relaxed", "Front Double Biceps", "Side Chest", "Side Triceps", "Rear Lat Spread", "Back Double Biceps", "Front Abs & Thigh", "Most Muscular"]
+
+poses.each do |pose|
+  Tag.create!(name: pose)
+end
