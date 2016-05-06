099d1fca59a3b1dc3d992f7a5b346e94937584a2
Seth Baughman <sbaughman1@me.com>
2016-05-06 14:38:32 -0400

    removes unnecessary methods from Contest

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,12 +1,4 @@
 class Contest < ApplicationRecord
   belongs_to :prep
   validates :title, :date, presence: true
-  # before_validation :format_time
-
-  def format_time
-    self.date += " 00:00:00"
-  end
 end
-
-# 05/19/2016
-# 2016-07-28 14:27:30
