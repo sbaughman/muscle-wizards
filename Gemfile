0e23b527431e6a330fc1e7b3ddbbe0954bbb9041
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-07 11:47:13 -0400

    Changed rspec-rails eating bacon version to 3.1, added factory girl methods in RSpec.configure, began writing user tests and having a default valid_user to test with

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ group :development, :test do
   gem 'factory_girl_rails', '~> 4.0'
   gem 'database_cleaner'
   gem 'capybara'
-  gem 'rspec-rails', '~> 3.0'
+  gem 'rspec-rails', '~> 3.1'
   gem "better_errors"
   gem "binding_of_caller"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:example) do
+    @user = @valid_user
+  end
+
+  it "is not valid without a name" do
+    @user.name = nil
+    expect(@user).not_to be_valid
+  end
+
+  it "is not valid without an email" do
+    @user.email = nil
+    expect(@user).not_to be_valid
+  end
+
+  it "is valid if name, email, age, and height are present, age and height are numbers, if email is unique and a valid format, and if password is present and has more than four unique characters" do
+    expect(@user).to be_valid
+  end
 end
