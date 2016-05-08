0cddfd3abb1d25d59c4e6b4308f103b52fea981a
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-08 10:40:53 -0400

    Organized invalid user tests into an invalid user context, valid user test into a valid user context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,19 +7,23 @@ RSpec.describe User, :type => :model do
   #   @user = FactoryGirl.build(:user)
   # end
 
-  it "is not valid without a name" do
-    @user = FactoryGirl.build(:user, name: nil)
-    expect(@user).not_to be_valid
+  context "invalid user" do
+    it "is not valid without a name" do
+      @user = FactoryGirl.build(:user, name: nil)
+      expect(@user).not_to be_valid
+    end
+
+    it "is not valid without an email" do
+      @user = FactoryGirl.build(:user, email: nil)
+      expect(@user).not_to be_valid
+    end
   end
 
-  it "is not valid without an email" do
-    @user = FactoryGirl.build(:user, email: nil)
-    expect(@user).not_to be_valid
-  end
-
-  it "is valid if name, email, age, and height are present, age and height are numbers, if email is unique and a valid format, and if password is present and has more than four unique characters" do
-    @user = FactoryGirl.build(:user)
-    expect(@user).to be_valid
+  context "valid user" do
+    it "is valid if name, email, age, and height are present, age and height are numbers, if email is unique and a valid format, and if password is present and has more than four unique characters" do
+      @user = FactoryGirl.build(:user)
+      expect(@user).to be_valid
+    end
   end
 end
 
