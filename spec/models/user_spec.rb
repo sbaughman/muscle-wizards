7ce101fd204b7b831d33b8f5463cd0f01719cab5
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-08 10:36:49 -0400

    Switched user model validationtests from a before(:example) hook to defining the user in each block using FactoryGirl.build(:user, attribute: nil) for the relevant attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  before(:example) do
-    # let(:user) { FactoryGirl.build(:user) } # I want to understand what the difference is between this and what I'm using
-    @user = FactoryGirl.build(:user)
-  end
+  # before(:example) do
+  #   # let(:user) { FactoryGirl.build(:user) } # I want to understand what the difference is between this and what I'm using
+  #   @user = FactoryGirl.build(:user)
+  # end
 
   it "is not valid without a name" do
-    @user.name = nil
+    @user = FactoryGirl.build(:user, name: nil)
     expect(@user).not_to be_valid
   end
 
   it "is not valid without an email" do
-    @user.email = nil
+    @user = FactoryGirl.build(:user, email: nil)
     expect(@user).not_to be_valid
   end
 
   it "is valid if name, email, age, and height are present, age and height are numbers, if email is unique and a valid format, and if password is present and has more than four unique characters" do
+    @user = FactoryGirl.build(:user)
     expect(@user).to be_valid
   end
 end
