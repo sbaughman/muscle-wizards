51a1f4b62af8fbe8093cdc482ead271b0488a769
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-08 10:44:06 -0400

    Changed existing factory definitions to blocks so that they generate new random information every time they are called

--- spec/factories/target_macros.rb
+++ spec/factories/target_macros.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :target_macro do
-    protein Random.new.rand(180..234)
-    carbs   Random.new.rand(170..200)
-    fat     Random.new.rand(58..71)
-    fiber   Random.new.rand(28..40)
-    prep    Prep.new(title: Faker::Superhero.name, user_id: build_stubbed(:user))
+    protein { Random.new.rand(180..234) }
+    carbs   { Random.new.rand(170..200) }
+    fat     { Random.new.rand(58..71) }
+    fiber   { Random.new.rand(28..40) }
+    prep    { Prep.new(title: Faker::Superhero.name, user_id: build_stubbed(:user)) }
   end
 end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    name      Faker::Superhero.name
-    email     Faker::Internet.email
-    age       Random.new.rand(18..45)
-    height    Random.new.rand(60..90)
-    gender    %w(M F).sample
-    password  Faker::Internet.password(8)
+    name      { Faker::Superhero.name }
+    email     { Faker::Internet.email }
+    age       { Random.new.rand(18..45) }
+    height    { Random.new.rand(60..90) }
+    gender    { %w(M F).sample }
+    password  { Faker::Internet.password(8) }
   end
 end
