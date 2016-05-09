13390c4b50754f60aae05c63e0b4648ee4b5d084
Ryder Timberlake <wrtimberlake@gmail.com>
2016-05-09 19:37:15 -0400

    Nested certification resources in user, now only the routes we need

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -1,15 +1,25 @@
 class CertificationsController < ApplicationController
 
   def new
+    @certification = Certification.new
   end
 
   def create
+    
   end
 
   def edit
+    @certification = Certification.find(params[:id])
   end
 
   def update
+
+  end
+
+  private
+
+  def certification_params
+
   end
 
 end
