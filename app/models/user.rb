d1126f06b428e6b43c163037c490e82eebdea507
Seth Baughman <sbaughman1@me.com>
2016-05-09 11:51:02 -0400

    adds cancan abilities to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include CanCan::Ability
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -17,8 +18,9 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-  def set_default_avatar   
-    self.avatar = URI.parse("https://unsplash.it/400/?image=1061") if avatar.blank?
+  def initialize(user)
+    can :manage, Prep, user_id: user.id
+    can :read, Prep, coach_id: user.id
   end
 
   private
@@ -33,6 +35,10 @@ class User < ApplicationRecord
 
   def password_might_not_be_completely_terrible
     errors.add(:password, "Password must contain at least four unique characters") unless self.password.split('').uniq.length >= 4
+  end
+
+  def set_default_avatar
+    self.avatar = URI.parse("https://unsplash.it/400/?image=1061") if avatar.blank?
   end
 
 end
