8ef3ff9b94c86d8cf8d39e349b5d7eb88b91d5d3
Seth Baughman <sbaughman1@me.com>
2016-05-09 15:13:03 -0400

    Added user index for coaches and user partial for coach data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :require_user
 
+  def index
+    @coaches = User.where(coach: true)
+  end
+
   def show
     if params[:id]
       @user = User.find(params[:id])
