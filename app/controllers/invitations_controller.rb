bf3b9136abfe272c887482b05200a484f831d79e
Seth Baughman <sbaughman1@me.com>
2016-05-10 10:51:38 -0400

    adds new, create, destroy controller actions for invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,31 @@
 class InvitationsController < ApplicationController
+
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+    @prep = Prep.find(params[:prep_id])
+    @invitation.user = User.find(params[:id])
+    @invitation.prep = @prep
+    if @invitation.save
+      flash[:success] = "Invitation sent!"
+      redirect_to prep_path(@prep)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @invitation = Invitation.find(params[:id])
+    @invitation.destroy
+    redirect_to prep_path(@invitation.prep)
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:message)
+  end
 end
