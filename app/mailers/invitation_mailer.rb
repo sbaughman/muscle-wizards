5d0046459fb0ea5ed50dbee88d7c76199dd8ee96
Seth Baughman <sbaughman1@me.com>
2016-05-10 11:55:43 -0400

    makes template for invitation email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,2 +1,9 @@
 class InvitationMailer < ApplicationMailer
+  default from: 'support@musclewizards.com'
+
+  def invite(recipient, prep)
+    @coach = recipient
+    @prep = prep
+    mail(to: @coach.email)
+  end
 end
