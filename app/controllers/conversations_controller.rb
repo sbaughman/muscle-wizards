dca344edb7191674b358034c71604c90ad78eba7
Seth Baughman <sbaughman1@me.com>
2016-05-11 11:41:38 -0400

    generates conversations controller and create action

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,19 @@
+class ConversationsController < ApplicationController
+  before_action :require_user
+
+  def create
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+    redirect_to conversation_messages_path(@conversation)
+  end
+
+  private
+
+  def conversation_params
+    params.permit(:recipient_id, :sender_id)
+  end
+
+end
