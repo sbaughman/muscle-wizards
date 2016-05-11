74983d9b0c64a66ab2148de43ab1e5e6da26d015
Seth Baughman <sbaughman1@me.com>
2016-05-11 09:41:22 -0400

    makes invitation_mailer methods for the athlete to be notified when a coach accepts their request, and for the coach to be notified if the athlete cancels a coaching request

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -7,4 +7,18 @@ class InvitationMailer < ApplicationMailer
     mail(to: @coach.email,
          subject: 'Request for Coaching')
   end
+
+  def accept(recipient, prep)
+    @athlete = recipient
+    @prep = prep
+    mail(to: @athlete.email,
+         subject: 'Coach Request Accepted!')
+  end
+
+  def reject(recipient, prep)
+    @coach = recipient
+    @prep = prep
+    mail(to: @coach.email,
+         subject: 'Coach Request Cancelled')
+  end
 end
