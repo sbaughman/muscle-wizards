d0588c2c589feb045c747fadddf68cf4d38140f5
Seth Baughman <sbaughman1@me.com>
2016-05-12 13:43:16 -0400

    adds avatar to allowed devise params, removes phone #, gender, age from signup, adds avatar to sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :height, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :height, :phone_number, :avatar])
   end
 end
