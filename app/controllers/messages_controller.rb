5f79f27e72c8516a6b10f4924ffb15db081b49c1
Seth Baughman <sbaughman1@me.com>
2016-05-17 14:13:30 -0400

    moves new message box to top of messages index. Fixes ordering to adjust to change

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,11 @@ class MessagesController < ApplicationController
     @messages = @conversation.messages.order(created_at: :asc)
     if @messages.length > 10
       @over_ten = true
-      @messages = @messages[-10..-1]
+      @messages = @messages[-10..-1].reverse
     end
     if params[:m]
       @over_ten = false
-      @messages = @conversation.messages.order(created_at: :asc)
+      @messages = @conversation.messages.order(created_at: :desc)
     end
     if @messages.last && @messages.last.user_id != current_user.id
       others_messages = @conversation.messages.where("user_id = ?", other_user(current_user))
