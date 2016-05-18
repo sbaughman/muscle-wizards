5cdfb0aaa3bfdb95c73dd30cd697e5c5d8055aa0
Seth Baughman <sbaughman1@me.com>
2016-05-18 13:09:42 -0400

    Validations limiting number on integer input fields

--- app/models/cardio.rb
+++ app/models/cardio.rb
@@ -1,5 +1,6 @@
 class Cardio < ApplicationRecord
   belongs_to :prep
-  validates :duration, :activity, :style, presence: true
-  validates :duration, numericality: true
+  validates :duration, :activity, presence: true
+  validates :duration, numericality: { greater_than: 0, less_than: 120, message: "Value must be between 1 and 120" }
+
 end
