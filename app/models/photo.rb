68920ed8b7416a10948770d83ea295971cb1f79f
Seth Baughman <sbaughman1@me.com>
2016-05-18 14:04:36 -0400

    Enforced consistent styling on all forms

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
   has_one :tag, through: :tagging
   has_one :tagging, dependent: :destroy
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }
-  validates :image, attachment_presence: true
+  validates :image, attachment_presence: { message: "You must attach a file" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 3.megabytes
 end
