a03976d8ec1204694fc2a4db5d0f4139cf4fb70b
Seth Baughman <sbaughman1@me.com>
2016-05-19 09:46:09 -0400

    adds validation to make sure certifications can 't be given a future date. Adds error message on new certification form for this. Fixes #60

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,4 +1,11 @@
 class Certification < ApplicationRecord
   belongs_to :user
   validates :name, :date_granted, presence: true
+  validate :is_in_the_future
+
+  def is_in_the_future
+    if self.date_granted && self.date_granted > Time.now
+      errors.add(:date_granted, "can't be a future date")
+    end
+  end
 end
