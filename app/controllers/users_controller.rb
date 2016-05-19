9eec5f11d1057c919bc69ffcb2aecd126197eeb1
Seth Baughman <sbaughman1@me.com>
2016-05-18 21:10:18 -0400

    adds option to change avatar in user edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :age, :gender, :height, :phone_number, :bio)
+    params.require(:user).permit(:name, :age, :gender, :height, :phone_number, :bio, :avatar)
   end
 
 end
