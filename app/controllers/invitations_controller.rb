a7e2ecb87744cd057228bf669b1f5374d8ca14b8
Seth Baughman <sbaughman1@me.com>
2016-05-20 10:31:10 -0400

    restricts user_owns_prep method to apply to only new and create actions in invitations controller since requested coach will not be an owner of the prep yet for the other actions

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class InvitationsController < ApplicationController
   before_action :set_prep
-  before_action :user_owns_prep
+  before_action :user_owns_prep, only: [:new, :create]
 
   def new
     @invitation = Invitation.new
